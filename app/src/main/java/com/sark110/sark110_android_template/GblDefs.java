0c84605a47c7d186280f0183ba47ad48ee1bda8f
Melchor Varela <37381609+EA4FRB@users.noreply.github.com>
2019-12-03 20:58:59 +0100

    Extended upper frequency range up to 1 GHz

--- app/src/main/java/com/sark110/sark110_android_template/GblDefs.java
+++ app/src/main/java/com/sark110/sark110_android_template/GblDefs.java
@@ -1,7 +1,7 @@
 package com.sark110.sark110_android_template;
 
 public class GblDefs {
-    static final int MAX_FREQ = 700000000;
+    static final int MAX_FREQ = 1000000000;
     static final int MIN_FREQ = 10000;
     static final int DEF_FREQ_START = 10100000;
 }
